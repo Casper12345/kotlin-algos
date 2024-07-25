a09f9453d8ce0beeaccf58061651ec534e4b48b7
Casper12345 <ctn@dixa.com>
2024-07-25 15:53:11 +0930

    occurrence

--- src/occurrence/Occurrence.kt
+++ src/occurrence/Occurrence.kt
@@ -18,9 +18,9 @@ object Occurrence {
 
 }
 
-object OccurrenceTernary {
-    object RunOccurrenceTernary {
-        fun runOccurrenceTernary(k: Int, a: Array<Int>, l: Int, r: Int): Int {
+object OccurrencePar {
+    object RunOccurrencePar {
+        fun runOccurrencePar(k: Int, a: Array<Int>, l: Int, r: Int): Int {
             var count = 0
             for (i in l until r) {
                 if (a[i] == k) {
@@ -31,33 +31,32 @@ object OccurrenceTernary {
         }
     }
 
-    fun run(k: Int, a: Array<Int>): Int {
-        // 0, (a.size)/3 = 3
-        // (a.size)/3, (a.size)/3 + (a.size)/3 = 3 = 6
-        // (a.size)/3 + (a.size)/3 + (a.size)/3
-        // [1,2,3,4] [5,6,7] [8,9,10]
-        var c1 = 0
-        var c2 = 0
-        var c3 = 0
+    fun run(k: Int, a: Array<Int>, par: Int): Int {
+        val  c = Array(par){0}
 
-        val m1 = (a.size)/3
-        val m2 = (a.size)/3 + (a.size)/3
+        val threads = ArrayList<Thread>()
 
-        val t1 = thread { c1 = RunOccurrenceTernary.runOccurrenceTernary(k, a, 0, m1) }
-        val t2 = thread { c2 = RunOccurrenceTernary.runOccurrenceTernary(k, a, m1, m2) }
-        val t3 = thread { c3 = RunOccurrenceTernary.runOccurrenceTernary(k, a, m2, a.size)}
-
-        t1.join()
-        t2.join()
-        t3.join()
-        return c1 +  c2  + c3 // [3,3,3,[1],2,3,[4],4,2,3,3]
+        for(i in 0 until par) {
+            if(i == 0){
+                val t = thread { c[i] = RunOccurrencePar.runOccurrencePar(k, a, 0, (a.size)/par ) }
+                threads.add(t)
+            } else if (i == par - 1){
+                val t = thread { c[i] = RunOccurrencePar.runOccurrencePar(k, a, (a.size/par) * i, a.size) }
+                threads.add(t)
+            } else {
+                val t = thread { c[i] = RunOccurrencePar.runOccurrencePar(k, a, (a.size/par) * i, (a.size/par) * (i + 1)) }
+                threads.add(t)
+            }
+        }
+        threads.forEach{t -> t.join()}
+        return c.sum()
     }
 
 }
 
 
 fun main() {
-    val seq = generateSequence { Random.nextInt(100)}.take(100000000).toList().toIntArray().toTypedArray()
+    val seq = generateSequence { Random.nextInt(100)}.take(1000000000).toList().toIntArray().toTypedArray()
     val t1 = Instant.now().toEpochMilli()
     val rs = Occurrence.occurrence(3, seq)
     val t2 = Instant.now().toEpochMilli()
@@ -66,7 +65,7 @@ fun main() {
     println(rs)
 
     val t3 = Instant.now().toEpochMilli()
-    val rs1 = OccurrenceTernary.run(3, seq)
+    val rs1 = OccurrencePar.run(3, seq, 6)
     val t4 = Instant.now().toEpochMilli()
     println(t4 - t3)
     println(rs1)
